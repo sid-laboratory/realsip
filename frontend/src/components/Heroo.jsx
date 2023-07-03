0285f428f9da6abe15aff5dec5ffaae096e9948a
B.SIDDHARTH <127313658+sid-laboratory@users.noreply.github.com>
2023-07-03 19:42:33 +0530

    f;ldf
    deploy

--- frontend/src/components/Heroo.jsx
+++ frontend/src/components/Heroo.jsx
@@ -10,11 +10,7 @@ return (
   <OurNav />
         <div className="text-black">
         <div className="flex flex-start justify-end pr-10 pb-4">
-            <Link to={'/login'}>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-4 border-b-4 p-4 border-blue-700 hover:border-blue-500 rounded">
-  Login
-</button>
-            </Link>
+           
             </div>
             <div className="max-w-[800px] mt-[-106px] text-4xl w-full h-screen mx-auto text-center flex flex-col justify-center">
          
@@ -23,6 +19,14 @@ return (
                 <p className="text-slate-100 p-4 text-3xl font-bold">Registrations open on </p>
                 <Typed className="text-white text-4xl font-bold"
                  strings={['SPORTS','NCC','EVENTS']} typeSpeed={100} backSpeed={100} loop/>
+                 
+            </div>
+            <div className="pt-10 ">
+            <Link to={'/login'}>
+            <button className="bg-gradient-to-r from-blue-900 to-teal-700  hover:bg-blue-400 text-white font-bold py-1 px-2 border-b-4 p-4 border-blue-900 hover:border-blue-700 rounded">
+  Login
+</button>
+            </Link>
             </div>
             
            
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,28 +2,17 @@
     import {AiOutlineClose , AiOutlineMenu} from 'react-icons/ai'
     import { Link } from 'react-router-dom';
    
-    
-    
-    
-    
     const Navbar = () =>
     { 
-        
         const [nav,setNav] = useState(true)
-
         const handleNav = () =>
         {
             setNav(!nav)
         }
-      
-
-
-
-
         return (
             
-            <div className="text-black  flex justify-between items-center h-24 max-w-[1240px] mx-auto px-2" >
-                <h1 className="w-full text-3xl font-bold text-white ">SPORTS ORGANISER.</h1>
+            <div className="text-black  flex justify-between items-center h-24 max-w-[1240px] mx-auto  " >
+                <h1 className="w-full text-3xl font-bold text-white   ">SPORTS ORGANISER.</h1>
                 <ul className="hidden"  >
 
                 <button className="p-4">HOME </button>
@@ -37,8 +26,9 @@
                                
                 </div>
                
-                <div className={!nav ? " fixed left-0 top-0 w-[30%] h-screen border-r border-r-gray-200 bg-stone-200 ease-in-out duration-500" : "fixed left-[-100%]  h-screen ease-in duration-500"}>
-                    <p className="w-full text-3xl font-bold text-stone-600 m-4 pl-20 pt-4 pointer-events-none">SPORTS ORGANISER</p>
+                <div className={!nav ? " fixed left-0 top-0 w-[30%] h-screen border-r max-w-[400px] border-r-gray-200 lg:pr-10 :text-[10px]  bg-stone-200 ease-in-out duration-500" : "fixed left-[-100%]  h-screen ease-in duration-500"}>
+                    
+                    <p className=" relative w-full text-3xl font-bold text-stone-600 m-4 pl-10 pt-4 pointer-events-none ">SPORTS ORGANISER</p>
                     <ul className="uppercase p-4">
                     <Link to='/' onClick={handleNav}> <button className="p-4  block border-b w-[100%] border-black  bold text-xl font-bold"> HOME </button> </Link>
                     <Link to='/Sports'> <button className="p-4 block border-b w-[100%] border-black text-xl font-bold">SPORTS</button>      </Link>
